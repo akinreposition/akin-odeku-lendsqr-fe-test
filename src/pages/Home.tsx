f75138a7676e82455289e6a1aeea67db30dfabfc
akinreposition <odekuakinlolu@gmail.com>
2024-10-31 19:28:35 +0100

    "Updated Home.tsx to add email and password input fields with state and event handlers, and modified Grid.scss and Home.scss to update styles for grid items and form inputs."

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import lendsqrLogo from "../assets/lendsqrLogo.svg"
 import pablo from "../assets/pablo.svg"
 
@@ -7,24 +7,60 @@ import '../styles/Grid.scss';
 import '../styles/Home.scss';
 
 const Home: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value.trim()); 
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value.trim()); 
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className='home-body'>
       <Grid>
-        <div className="grid-item grid-item-hid">
+        <div className="grid-item1 grid-item-hid">
           <div className='logo'>
-            <img src={lendsqrLogo} alt="logo" />
+            <img src={lendsqrLogo}  alt="logo" />
           </div>
-          <img src={pablo} alt="logo" />
+          <img src={pablo} width={500} alt="logo" />
         </div>
         <div className="grid-item">
           <div className="form">
           <h2 className='form header'>Welcome!</h2>
           <h4 className='form header subTitle'>Enter details to login.</h4>
           <form>
-          <div class="form-input">
-              <input type="text" v-model.trim="atmTerminalID" @input="handleSearch"/>
-              <label for="Atm Terminal ID">Atm Terminal ID</label>
-          </div>
+            <div className="form-input">
+              <input
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-input">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={handlePasswordChange}
+                placeholder="Password"
+                required
+              />
+              <span className="password-toggle" onClick={togglePasswordVisibility}>
+                {showPassword ? 'HIDE' : 'SHOW'}
+              </span>
+            </div>
+            <h5 className='forget-password'>FORGOT PASSWORD</h5>
+            <div className="form-input">
+              <button className='login'>log in</button>
+            </div>
           </form>
           </div>
         </div>
